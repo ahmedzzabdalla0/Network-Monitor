e923e425b7670b1084890e2708a380ab2ee628c2
A .M <ahmedzzabdalla0@gmail.com>
2025-11-06 09:19:14 +0200

    Enhance logging

--- src/wlan/logging_runner.py
+++ src/wlan/logging_runner.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 logging_file = os.path.join(BASE_PATH, "running.log")
 logging_level = int(ConfigManager.get("main.logging_level", 20))
-loggin_format = '%(asctime)s - %(levelname)s - %(message)s'
+loggin_format = '%(asctime)s - %(levelname)s - [%(filename)s:%(lineno)d] - %(message)s'
 
 if os.path.exists(logging_file):
     os.remove(logging_file)
